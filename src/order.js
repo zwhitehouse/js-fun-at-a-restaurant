60e72e82b2498c0ec60f530409cee66b010ac087
Zack Whitehouse <zackwhitehouse225@gmail.com>
2023-08-30 18:35:26 -0400

    added answers

--- src/order.js
+++ src/order.js
@@ -1,7 +1,42 @@
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
+}
+
+
+function takeOrder(order, orderArr) {
+  if (orderArr.length < 3) {
+    orderArr.push(order)
+  }
+  return orderArr;
+}
+
+function refundOrder(orderNumber, orderArr) {
+  for (var i = 0; i < orderArr.length; i++) {
+    if (orderNumber === orderArr[i].orderNumber) {
+      orderArr.splice(i, 1); // Remove 1 element at index i
+      return true; // Indicate successful removal
+    }
+  }
+  return false; // Indicate orderNumber was not found
+}
+
+function listItems(orderArr) {
+  var itemNames = [];
+  for (var i = 0; i < orderArr.length; i++) {
+    itemNames.push(orderArr[i].item);
+  }
+  return itemNames.join(", ");
+}
+
+function searchOrder(orderArr, itemName) {
+  for (var i = 0; i < orderArr.length; i++) {
+    if (orderArr[i].item === itemName) {
+      return true;
+    }
+  }
+  return false;
 }
--- src/restaurant.js
+++ src/restaurant.js
@@ -2,12 +2,51 @@ function createRestaurant(name) {
   var pizzaRestaurant = {
     name: name,
     
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  };
+return pizzaRestaurant;
+};
+
+function addMenuItem(restaurant, item) {
+  if (!restaurant.menus[item.type].includes(item))
+  restaurant.menus[item.type].push(item);
+};
+//NEED bracket notation to make function dynamic and access 'type' of variable.
+
+function removeMenuItem(restaurant, item, type) {
+  var menu = restaurant.menus[type];
+  for (var i = 0; i < menu.length; i++) {
+    if (menu[i].name === item) {
+      menu.splice(i, 1);
+      return `No one is eating our ${item} - it has been removed from the ${type} menu!`;
+    }
   }
+  return `Sorry, we don't sell ${item}, try adding a new recipe!`;
 }
+
+function checkForFood(restaurant, requestedItem) {
+  var menu = restaurant.menus[requestedItem.type];
+  for (var i = 0; i < menu.length; i++) {
+    if (menu[i].name === requestedItem.name) {
+      return "Yes, we're serving " + requestedItem.name + " today!";
+    }
+  }
+  return "Sorry, we aren't serving " + requestedItem.name + " today.";
+}
+//This one was a BITCH! 
+
+
+
+
+
 
 module.exports = {
   createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem,
-  // checkForFood
+  addMenuItem,
+  removeMenuItem,
+  checkForFood
 }
